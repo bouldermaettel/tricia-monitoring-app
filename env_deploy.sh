1670dd283d095e4eacd8020fe67228f3787c54d2
bouldermaettel <bouldermaettel@gmail.com>
2026-05-21 12:36:40 +0200

    added matrix logic

---
+++ env_deploy.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY='theoneandonlysecretkeyfortriciamonitoring$1'
+export BOOTSTRAP_ADMIN_USERNAME='tricia.admin'
+export BOOTSTRAP_ADMIN_PASSWORD='swissmedic$01'
+export POSTGRES_ADMIN_PASSWORD='swissmedic$01'
